899be68e5740fa5811f1f31063b62ca47c14c48a
Kareem Amara <kareemamarah2356@gmail.com>
2023-08-31 20:47:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ReactLoading from "react-loading";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+  const list = [
+    {
+      prop: "spinningBubbles",
+      name: "SpinningBubbles",
+    },
+  ];
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 xsm:grid-cols-1  gap-4">
+          {isLoading ? (
+            <div className="flex justify-center items-center h-[100vh] w-[100vw] mx-auto">
+              <ReactLoading type={list.prop} color="black" />
+            </div>
+          ) : (
+            images.map((image) => (
+              <div className="mx-auto">
+                <ImageCard key={image.id} image={image} />
+              </div>
+            ))
+          )}
+        </div>
+      }
     </div>
   );
 }
